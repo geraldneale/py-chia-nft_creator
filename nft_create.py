51185af89db32ec65d97b1ba7ebf98651cddf3ff
GERALD D NEALE <53587595+geraldneale@users.noreply.github.com>
2022-07-10 08:24:28 -0400

    Update nft_create.py

--- nft_create.py
+++ nft_create.py
@@ -2,21 +2,25 @@ import subprocess
 import hashlib
 import requests
 
+fprint = "790396907"
 wallet = 4
 originals_path = "/home/gneale/Programming/nft0/files/originals/"
+royalty_address = "txch150trmj9g08555k3qaptn0sl5dseq0recwmvgn73cdtch2dc3t0ksjuespy"
+target_address = "txch150trmj9g08555k3qaptn0sl5dseq0recwmvgn73cdtch2dc3t0ksjuespy"
 
-def create_nft(num, nlink, mlink, wallet):
+
+def create_nft(num, nlink, mlink, wallet, nhash, mhash):
     
     try:
         result = subprocess.run(["chia", "wallet", "nft", "mint", 
-        "-f","790396907",
+        "-f", fprint,
         "-i", str(wallet),
-        "-ra", "txch150trmj9g08555k3qaptn0sl5dseq0recwmvgn73cdtch2dc3t0ksjuespy",
-        "-ta", "txch150trmj9g08555k3qaptn0sl5dseq0recwmvgn73cdtch2dc3t0ksjuespy",
+        "-ra", royalty_address,
+        "-ta", target_address,
         "-u", nlink,
-        "-nh", "cfb48193d9c8ab7c9585c9653430c95c5fc0d1d17aa4a45aad63548602eb47d7", 
+        "-nh", nhash, 
         "-mu", mlink,
-        "-mh", "f7fb3c5fd263a3b0845c0122555179f3ceb6a3091da48065df90aaea86531118",
+        "-mh", mhash,
         "-lu", "https://raw.githubusercontent.com/Chia-Network/chia-blockchain/main/LICENSE",
         "-lh", "30a358857da6b49f57cfe819c1ca43bfe007f528eb784df5da5cb64577e0ffc6", 
         "-sn", str(num), "-st", "726", 
@@ -55,15 +59,21 @@ def get_local_hash(image_number, originals_path, filename):
           return readable_hash                        
 
 if __name__=='__main__':
-    for num in range(10,100):
+    for num in range(20,100):
         for ext in ["png","json"]:
             filename = "gneale-san_francisco{}.{}".format(num, ext)
             url = "https://mojopuzzler.org/nft/gnsf/gneale-san_francisco{}.{}".format(num, ext)
-            if get_online_hash(num, url, filename) != get_local_hash(num, originals_path, filename):
+            online_hash = get_online_hash(num, url, filename)
+            local_hash = get_local_hash(num, originals_path, filename)
+            if online_hash != local_hash:
                 print("{} file number {} hash does NOT successfully match.".format(ext,num))
+            if ext == "png":
+                nhash = online_hash
+            else:
+                mhash = online_hash        
 
         yn = yes_or_no("Would you like to make NFT # {}".format(num))
         if yn:
             nlink = "https://mojopuzzler.org/nft/gnsf/gneale-san_francisco{}.png".format(num)
             mlink = "https://mojopuzzler.org/nft/gnsf/gneale-san_francisco{}.json".format(num)
-            create_nft(num, nlink, mlink, wallet)
+            create_nft(num, nlink, mlink, wallet, nhash, mhash)
